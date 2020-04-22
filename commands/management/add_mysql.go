4b4a4ecd45a6f88bca53e4e94e27579bd5b8ac68
Nurlan Moldomurov <nurlan.moldomurov@percona.com>
2020-04-22 13:04:26 +0300

    PMM-5695 PMM-5696 Fix issue with ignoring errors.

--- commands/management/add_mysql.go
+++ commands/management/add_mysql.go
@@ -125,11 +125,15 @@ func (cmd *addMySQLCommand) processGlobalAddFlags() (serviceName string, socket 
 
 	socket = cmd.Socket
 	address := cmd.GetAddress()
-	var portI int
 	if socket == "" {
 		if address == "" {
 			address = "127.0.0.1:3306"
 		}
+	}
+
+	var portI int
+
+	if address != "" {
 		var portS string
 		host, portS, err = net.SplitHostPort(address)
 		if err != nil {
@@ -140,14 +144,14 @@ func (cmd *addMySQLCommand) processGlobalAddFlags() (serviceName string, socket 
 		if err != nil {
 			return "", "", "", 0, err
 		}
+	}
 
-		if *addHostFlag != "" {
-			host = *addHostFlag
-		}
+	if *addHostFlag != "" {
+		host = *addHostFlag
+	}
 
-		if *addPortFlag != 0 {
-			portI = int(*addPortFlag)
-		}
+	if *addPortFlag != 0 {
+		portI = int(*addPortFlag)
 	}
 
 	return serviceName, socket, host, uint16(portI), nil
