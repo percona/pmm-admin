0b0365ce753e729cb1b526c15d4e1e20f4f62d75
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-02-15 12:03:57 +0100

    Revert "Merge branch 'master' into PMM-6797-cli-for-haproxy"
    This reverts commit c1c699a9463bc739b5d094124adc40da61bd9678.

--- commands/management/add_external_serverless.go
+++ commands/management/add_external_serverless.go
@@ -20,6 +20,7 @@ import (
 	"net"
 	"net/url"
 	"strconv"
+	"strings"
 
 	"github.com/AlekSi/pointer"
 	"github.com/percona/pmm/api/managementpb/json/client"
@@ -183,7 +184,7 @@ func init() {
 	AddExternalServerlessC.Flag("username", "External username").StringVar(&AddExternalServerless.Username)
 	AddExternalServerlessC.Flag("password", "External password").StringVar(&AddExternalServerless.Password)
 
-	AddExternalServerlessC.Flag("scheme", "Scheme to generate URL to exporter metrics endpoints").StringVar(&AddExternalServerless.Scheme)
+	AddExternalServerlessC.Flag("scheme", "Scheme to generate URL to exporter metrics endpoints (http, https)").StringVar(&AddExternalServerless.Scheme)
 	AddExternalServerlessC.Flag("url", "Full URL to exporter metrics endpoints").StringVar(&AddExternalServerless.URL)
 	AddExternalServerlessC.Flag("address", "External exporter address and port").StringVar(&AddExternalServerless.Address)
 	AddExternalServerlessC.Flag("host", "External exporters hostname or IP address").StringVar(&AddExternalServerless.Host)
