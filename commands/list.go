84217d81518869ba7c20ede8f224e2a91dfd75f1
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2021-02-17 09:47:52 +0100

    PMM-6797 List command.

--- commands/list.go
+++ commands/list.go
@@ -159,19 +159,19 @@ func (cmd *listCommand) Run() (Result, error) {
 			AddressPort: getAddressPort(s.Socket, s.Address, s.Port),
 		})
 	}
+	for _, s := range servicesRes.Payload.Haproxy {
+		servicesList = append(servicesList, listResultService{
+			ServiceType: types.ServiceTypeHAProxyService,
+			ServiceID:   s.ServiceID,
+			ServiceName: s.ServiceName,
+		})
+	}
 	for _, s := range servicesRes.Payload.External {
 		servicesList = append(servicesList, listResultService{
 			ServiceType: types.ServiceTypeExternalService,
 			ServiceID:   s.ServiceID,
 			ServiceName: s.ServiceName,
 			Group:       s.Group,
-		})
-	}
-	for _, s := range servicesRes.Payload.Haproxy {
-		servicesList = append(servicesList, listResultService{
-			ServiceType: types.ServiceTypeHAProxyService,
-			ServiceID:   s.ServiceID,
-			ServiceName: s.ServiceName,
 		})
 	}
 
