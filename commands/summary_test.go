f53a72aa79caffbbe18bbc5a2f1117901d5981b9
Jiri Ctvrtka <jiri.ctvrtka.sde@gmail.com>
2022-01-04 11:11:11 +0100

    PMM-5831 Const for default port.

--- commands/summary_test.go
+++ commands/summary_test.go
@@ -29,8 +29,10 @@ import (
 	"github.com/percona/pmm-admin/agentlocal"
 )
 
+const defaultPMMAgentListenPort = 7777
+
 func TestSummary(t *testing.T) {
-	agentlocal.SetTransport(context.TODO(), true, 7777)
+	agentlocal.SetTransport(context.TODO(), true, defaultPMMAgentListenPort)
 
 	f, err := ioutil.TempFile("", "pmm-admin-test-summary")
 	require.NoError(t, err)
